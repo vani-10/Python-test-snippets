6445c206bc8cc1745fd713059515789a3ee298b5
vani-10 <kavipillaiph@gmail.com>
2019-12-24 18:07:53 +0100

    two code

---
+++ test_materials.py
@@ -0,0 +1,21 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> for i in range(-5,6)
+SyntaxError: invalid syntax
+>>> for i in range(-5,6):print(i)
+
+-5
+-4
+-3
+-2
+-1
+0
+1
+2
+3
+4
+5
+>>> 
